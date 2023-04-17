41dfbc9f38ed3311b3da766f13eaae1627d55366
spacekimchis <spacekimchis@gmail.com>
2023-04-17 13:32:50 -0700

    rename executable

--- traders-watch.sh
+++ traders-watch.sh
@@ -7,7 +7,7 @@ inotifywait -m -e create -e moved_to --format "%f" $TARGET \
         | while read FILENAME
                 do
                         echo Detected $FILENAME, moving and updating database
-						./traders-watch "$TARGET" "$FILENAME" | psql -h 0.0.0.0 -p 5432 -U postgres -d traders
+						./trademaker "$TARGET" "$FILENAME" | psql -h 0.0.0.0 -p 5432 -U postgres -d traders
 						echo Processed files $TARGET/$FILENAME into database
                         rm -rf "$TARGET/$FILENAME"
                 done
