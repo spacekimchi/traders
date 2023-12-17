2d9180ab0861a19da80f88ed244268a49d4b4981
spacekimchi <spacekimchis@gmail.com>
2023-12-17 11:35:19 -0800

    some edits

--- src/startup.rs
+++ src/startup.rs
@@ -116,6 +116,8 @@ pub async fn run(db_pool: PgPool, listener: TcpListener, base_url: String, redis
             .service(
                 web::scope("/api")
                 .service(api::health_check::health_check)
+                .service(api::health_check::health_check_posts)
+                .service(api::executions::ninja_trader_executions_import)
                 .service(api::accounts::list)
                 .service(api::users::current_user)
                 .service(
