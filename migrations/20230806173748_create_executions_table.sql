220db6db4ef5a7a9eb6ac503d1c0137d5dd71021
spacekimchi <spacekimchis@gmail.com>
2023-12-17 11:35:19 -0800

    some edits

--- migrations/20230806173748_create_executions_table.sql
+++ migrations/20230806173748_create_executions_table.sql
@@ -3,11 +3,17 @@ CREATE TABLE IF NOT EXISTS executions
     id BIGSERIAL NOT NULL PRIMARY KEY,
     trade_id BIGSERIAL references trades (id) ON DELETE CASCADE,
     instrument_id SERIAL references instruments (id),
-    fill_time DOUBLE PRECISION NOT NULL,
+    account_id SERIAL references accounts (id),
+    fill_time TIMESTAMPTZ NOT NULL,
+    order_id TEXT NOT NULL,
     commissions REAL,
     price REAL NOT NULL,
-    is_buy BOOLEAN NOT NULL, -- TRUE for 'buy', FALSE for 'sell'
     quantity INTEGER NOT NULL,
+    is_buy BOOLEAN NOT NULL, -- TRUE for 'buy', FALSE for 'sell'
+    is_initial_entry BOOLEAN NOT NULL,
+    is_last_exit BOOLEAN NOT NULL,
+    is_entry BOOLEAN NOT NULL,
+    is_exit BOOLEAN NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
