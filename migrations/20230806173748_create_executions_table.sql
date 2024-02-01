6d1bd35153f14a39878bd120fb6b2edc13b4642f
spacekimchi <spacekimchis@gmail.com>
2024-01-31 21:46:21 -0500

    add fields to execution

--- migrations/20230806173748_create_executions_table.sql
+++ migrations/20230806173748_create_executions_table.sql
@@ -9,6 +9,10 @@ CREATE TABLE IF NOT EXISTS executions
     commission REAL,
     price REAL NOT NULL,
     quantity INTEGER NOT NULL,
+    is_initial_entry BOOLEAN NOT NULL,
+    is_entry BOOLEAN NOT NULL,
+    is_last_exit BOOLEAN NOT NULL,
+    is_exit BOOLEAN NOT NULL,
     is_buy BOOLEAN NOT NULL, -- TRUE for 'buy', FALSE for 'sell'
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
