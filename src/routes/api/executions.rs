800aab2840faaf2ea38742bf35797b02fd088efa
spacekimchi <spacekimchis@gmail.com>
2024-02-07 22:35:57 -0500

    changes

--- src/routes/api/executions.rs
+++ src/routes/api/executions.rs
@@ -4,17 +4,26 @@ use serde::Deserialize;
 
 use crate::startup::AppState;
 
+// Sample executions that will be sent from the NinjaTrader backend
+// [
+     //{"id":"ea1efabf68cf4be9a3dc344b63e5edf2","account_name":"Sim101","order_id":"ef41669c50854e8fad5dd5bfd766cd6a","instrument_name":"ES","fill_time":45329.840480127314,"commission":2.09,"price":5012.25,"position":1,"is_buy":true,"quantity":1,"is_entry":true,"is_exit":false},
+     //{"id":"48c3dbc37fe242c490884f1340143909","account_name":"Sim101","order_id":"ede9aedac1a14d98ab255e64a9cbe467","instrument_name":"ES","fill_time":45329.840523148145,"commission":2.09,"price":5012.25,"position":0,"is_buy":false,"quantity":1,"is_entry":false,"is_exit":true}
+// ]
+
 #[derive(Debug, Deserialize)]
 pub struct ExecutionJsonData {
-    id: i64,
+    id: String,
     account_name: String,
     order_id: String,
     instrument_name: String,
     fill_time: f64, // Need to convert this to TIMESTAMPTZ
     commission: f32,
     price: f32,
+    position: i32,
     is_buy: bool,
     quantity: i64,
+    is_entry: bool,
+    is_exit: bool,
 }
 
 #[post("/ninja_trader_executions_import")]
