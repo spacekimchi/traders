cc236a01f719f33ad320d01eee0cac452cb28553
spacekimchi <spacekimchis@gmail.com>
2024-03-20 11:23:05 -0300

    fix mes and mnq price per tick

--- src/instrument_prices.rs
+++ src/instrument_prices.rs
@@ -16,9 +16,9 @@ pub fn by_symbol(symbol: &str) -> (f32, f32) {
 
 /// (i32, f32) = (ticks in a point, price per tick)
 pub const ES: (f32, f32) = (4.0, 12.5);
-pub const MES: (f32, f32) = (4.0, 12.5);
-pub const NQ: (f32, f32) = (4.0, 12.5);
-pub const MNQ: (f32, f32) = (4.0, 12.5);
+pub const MES: (f32, f32) = (4.0, 1.25);
+pub const NQ: (f32, f32) = (4.0, 5.0);
+pub const MNQ: (f32, f32) = (4.0, 0.5);
 pub const GC: (f32, f32) = (10.0, 0.1);
 pub const MGC: (f32, f32) = (10.0, 0.1);
 
